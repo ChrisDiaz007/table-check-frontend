9d4097c81689329a0687534cfaa51306c5aace29
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-29 14:13:35 -0400

    Can send patch with axios to backend to update user account, tested and working, will css it later

--- src/Components/User/UpdateProfile.jsx
+++ src/Components/User/UpdateProfile.jsx
@@ -5,10 +5,16 @@ import { useParams, useNavigate } from "react-router-dom";
 import TokenStore from "../../Auth/TokenStore";
 
 const UpdateProfile = () => {
-  const { id } = useParams();
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const { id } = useParams();
 
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState({
+    first_name: "",
+    last_name: "",
+  });
+
+  // Load User
   useEffect(() => {
     const token = TokenStore.getAccessToken(); // if you're using JWTs
     axios
@@ -25,12 +31,41 @@ const UpdateProfile = () => {
   }, [id, navigate]);
   if (!user) return <div>Loading...</div>;
 
+  // Update User
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    setProfile((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    const token = TokenStore.getAccessToken();
+    event.preventDefault();
+    axios
+      .patch(
+        `http://localhost:3000/api/v1/users/${id}`,
+        { user: profile },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => console.log("Form Submitted Successfully", response))
+      .catch((error) => console.log("Error submitting form", error));
+  };
+
   return (
+    // prettier-ignore
     <div>
-      <p>test profile</p>
-      <p>{user.first_name}</p>
-      <p>{user.last_name}</p>
-      <p>test</p>
+      <p>Update profile</p>
+      
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        First Name : <input type="text" onChange={handleInput} name="first_name" />
+        Last Name : <input type="text" onChange={handleInput} name="last_name" />
+        <button type="Submit">Update Account</button>
+      </form>
     </div>
   );
 };
