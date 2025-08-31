6e81b5ff0ec37d79c9df1acdbbd1debceac37166
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-08-31 14:54:20 -0400

    Restaurant New CSS, form looks better

--- src/Components/Content/RestaurantNew/RestaurantNew.jsx
+++ src/Components/Content/RestaurantNew/RestaurantNew.jsx
@@ -62,27 +62,36 @@ const RestaurantNew = () => {
 
   // prettier-ignore
   return (
-    <div className="">
-      <div>
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          Name : <input type="text" onChange={handleInput} name="name" />
-          Address : <input type="text" onChange={handleInput} name="address" />
-          Prefecture : <input type="text" onChange={handleInput} name="prefecture" />
-          District : <input type="text" onChange={handleInput} name="district" />
-          About : <input type="text" onChange={handleInput} name="about" />
-          Description : <input type="text" onChange={handleInput} name="description" />
-          Phone Number : <input type="text" onChange={handleInput} name="phone_number" />
-          Website : <input type="text" onChange={handleInput} name="website" />
-          Total Tables : <input type="text" onChange={handleInput} name="total_tables" />
-          Lunch Price : <input type="text" onChange={handleInput} name="lunch_price" />
-          Dinner Price : <input type="text" onChange={handleInput} name="dinner_price" />
-          Photo : <input type="file" onChange={handleFileInput} name="photo" accept="image/*" required />
-          <button type="submit">
-            Create Restaurant
+    <section className="Restaurant-New flex justify-center">
+      <div className="w-125 flex flex-col pt-10">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-1">
+          <p className="font-semibold text-3xl">New Restaurant Form</p>
+          Name : <input type="text" onChange={handleInput} name="name" className="border rounded-md p-2"/>
+          Address : <input type="text" onChange={handleInput} name="address" className="border rounded-md p-2"/>
+          Prefecture : <input type="text" onChange={handleInput} name="prefecture" className="border rounded-md p-2"/>
+          District : <input type="text" onChange={handleInput} name="district" className="border rounded-md p-2"/>
+          About : <input type="text" onChange={handleInput} name="about" className="border rounded-md p-2"/>
+          Description : <textarea
+                          onChange={handleInput}
+                          name="description"
+                          rows={4}
+                          className="border rounded-md w-full h-24 p-2 resize-y align-top"
+                        />
+          Phone Number : <input type="text" onChange={handleInput} name="phone_number" className="border rounded-md p-2"/>
+          Website : <input type="text" onChange={handleInput} name="website" className="border rounded-md p-2"/>
+          Total Tables : <input type="text" onChange={handleInput} name="total_tables" className="border rounded-md p-2"/>
+          Lunch Price : <input type="text" onChange={handleInput} name="lunch_price" className="border rounded-md p-2"/>
+          Dinner Price : <input type="text" onChange={handleInput} name="dinner_price" className="border rounded-md p-2"/>
+          Photo : <input type="file" onChange={handleFileInput} name="photo" accept="image/*" required className="border rounded w-55 p-2"/>
+          
+          <span>
+            <button type="submit" className="bg-purple-300 p-3 rounded-md">
+            Create New Restaurant
           </button>
+          </span>
         </form>
       </div>
-    </div>
+    </section>
   );
 };
 
